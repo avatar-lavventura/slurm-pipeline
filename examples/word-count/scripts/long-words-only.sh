d99ec4681555264ad6a4a54c8a77749c1cccbe87
avatar-lavventura <alper.alimoglu@gmail.com>
2018-10-17 17:34:53 +0300

    Update long-words-only.sh

--- examples/word-count/scripts/long-words-only.sh
+++ examples/word-count/scripts/long-words-only.sh
@@ -7,7 +7,5 @@ echo "Args are $@" > $out
 echo "SP_* env vars are:" >> $out
 set | egrep '^SP_' | sed 's/^/  /' >> $out
 
-awk 'length($0) > 5' < output/$task.words > output/$task.long-words
-
-# Pretend that we started a SLURM job to do more work on this task.
-echo "TASK: $task $RANDOM"
+jobid=$(sbatch $SP_DEPENDENCY_ARG $SP_NICE_ARG scripts/one-word-per-line_script.sh $task | cut -f4 -d' ')
+echo TASK: $task $jobid
